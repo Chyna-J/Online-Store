47a2438e23ef347e827c09bf702281007a2f15a6
Chyna <51467858+Chyna-J@users.noreply.github.com>
2023-11-30 11:08:47 -0500

    Test

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
 
 if (bar) {
-    bar.addEventListener ('click', () => {
-        nav.classList.add('active');
+    bar.addEventListener ("click", () => {
+        onclick = () => {
+            nav.classList.add('active')
+        };     
     })
 }
 
 if (close){
-    bar.addEventListener('click', () => {
-        nav.classList.remove('active');
+    bar.addEventListener("click", () => {
+        onclick = () => {
+            nav.classList.remove('active')
+        };
     })
 }
